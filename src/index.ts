cace8d057eea27a677b931b8aaa0df3b190b0d94
Satnam Singh Brar <satnam@zaber.com>
2021-10-07 09:01:52 -0700

    feat: can now send a HELLO message to the WAMP router
    - Changed the arguments to the constructor so that it now expects an
      object to be provided rather than separate arguments.
      - The object also requires us to provide `authid` and `authmethods`
        now
    - Defined an enum for the supported authmethods
    - Defined an enum for the message types that the WAMP protocol
      recognizes
    - After the websocket connection is established, the message handler
      gets setup (which currently just console logs the event data), and a
      basic HELLO message is sent to the router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,57 @@
+enum AuthMethod {
+  Cookie = 'cookie',
+  Ticket = 'ticket',
+  Anonymous = 'anonymous',
+}
+
+enum MessageTypes {
+  Hello = 1,
+  Welcome = 2,
+  Abort = 3,
+  Challenge = 4,
+  Authenticate = 5,
+  Goodbye = 6,
+  Error = 8,
+
+  Publish = 16,
+  Published = 17,
+
+  Subscribe = 32,
+  Subscribed = 33,
+  Unsubscribe = 34,
+  Unsubscribed = 35,
+
+  Event = 36,
+
+  Call = 48,
+  Result = 50,
+
+  Register = 64,
+  Registered = 65,
+  Unregister = 66,
+  Unregistered = 67,
+
+  Invocation = 68,
+  Yield = 70,
+}
+
+interface SwampyerOptions {
+  url: string;
+  realm: string;
+  authid: string;
+  authmethods: AuthMethod[]
+}
+
 class Swampyer {
   private socket: WebSocket;
 
-  constructor(public readonly url: string, public readonly realm: string) {
+  constructor(private readonly options: SwampyerOptions) {
     console.log('SWAMPYER CONSTRUCTOR');
     this.setupConnection();
   }
 
-  async setupConnection() {
-    this.socket = new WebSocket(this.url, ['wamp.2.json']);
+  private async setupConnection() {
+    this.socket = new WebSocket(this.options.url, ['wamp.2.json']);
 
     await new Promise<void>((resolve, reject) => {
       // TODO cleanup these event listeners after we are done with them
@@ -22,5 +66,25 @@ class Swampyer {
         reject();
       })
     });
+
+    this.socket.addEventListener('message', this.messageHandler);
+
+    this.socket.send(JSON.stringify([MessageTypes.Hello, this.options.realm, {
+      authid: this.options.authid,
+      agent: 'swampyer-js',
+      authmethods: this.options.authmethods || ['anonymous'],
+      roles: {
+        subscriber: {},
+        publisher: {},
+        caller: {},
+        callee: {},
+      }
+    }]));
+  }
+
+  private messageHandler(event: MessageEvent<string>) {
+    console.log('SOCKET MESSAGE EVENT', event);
+    const data = JSON.parse(event.data);
+    console.log('SOCKET MESSAGE DATA', data);
   }
 }
