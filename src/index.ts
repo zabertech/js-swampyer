41c6acb1b6277201bb483ddf285168b5c5abd3b9
Satnam Singh Brar <satnam@zaber.com>
2021-10-06 21:29:05 -0700

    feat: can now make a websocket connection to a WAMP server
    - Can't do anything useful with it yet though

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 class Swampyer {
-  constructor() {
+  private socket: WebSocket;
+
+  constructor(public readonly url: string, public readonly realm: string) {
     console.log('SWAMPYER CONSTRUCTOR');
+    this.setupConnection();
+  }
+
+  async setupConnection() {
+    this.socket = new WebSocket(this.url, ['wamp.2.json']);
+
+    await new Promise<void>((resolve, reject) => {
+      // TODO cleanup these event listeners after we are done with them
+
+      this.socket.addEventListener('open', e => {
+        console.log('SOCKET OPEN', e);
+        resolve();
+      });
+
+      this.socket.addEventListener('error', e => {
+        console.log('SOCKET ERROR', e);
+        reject();
+      })
+    });
   }
 }
