35496f1e6a779f8f7489010462c276123be676db
Satnam Singh Brar <satnam@zaber.com>
2021-10-11 13:23:52 -0700

    feat: Setup testing
    - I have put together a general setup for writing tests
      - Decided to go with `mocha` as the testing framework and `chai`
        for the assertions
      - `ts-node` is used for running the tests written in typescript
    - The `src/transports/transports.test.ts` tests are working even
      though they don't really do anything

---
+++ src/transports/transport.test.ts
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+describe('Transport', () => {
+  it('should be simple', async () => {
+
+  });
+});
