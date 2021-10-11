19c5fe71a3b3e5b1185ff0d0d1b868d277e94d00
Satnam Singh Brar <satnam@zaber.com>
2021-10-11 14:26:36 -0700

    feat: replaced `mocha` and `chai` with `jest`
    - I thought that using mocha for this library would be nice but I needed
      some things like `jest.fn()` that `chai` and `mocha` just didn't have
      easily available
      - So, I have switched to using `jest` instead
    - I have written descriptions for the `Transport` tests I mean to
      implement. But they don't work yet

--- src/transports/transport.test.ts
+++ src/transports/transport.test.ts
@@ -1,10 +1,19 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-
-chai.use(chaiAsPromised);
+import { Transport } from './transport';
 
 describe('Transport', () => {
-  it('should be simple', async () => {
-
+  describe('open()', () => {
+    it('emits an event once the transport is open and ready for use', async () => {});
+  });
+  describe('read()', () => {
+    it('returns a promise that only resolves when when "_send()" is called', async () => {});
+    it('throws an error if the transport is closed', async () => {});
+  });
+  describe('write()', () => {
+    it('emits the "message" event with the same data that "write()" is called with', async () => {});
+  });
+  describe('close()', () => {
+    it('emits the "close" event if closed without error', async () => {});
+    it('emits the "close" event and associated error if closed with error', async () => {});
+    it('rejects any ongoing "read()" operations', async () => {});
   });
 });
